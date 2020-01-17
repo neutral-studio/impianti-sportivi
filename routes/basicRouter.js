2766d7b3a15f1adabed23da3172ea270db2effae
Deblazz <gioco654@gmail.com>
2020-01-17 11:54:18 +0100

    added basic routes

--- routes/basicRouter.js
+++ routes/basicRouter.js
@@ -6,4 +6,16 @@ const basicController = require('./../controllers/basicController');
 
 router.route('/').get(basicController.get_home);
 
+
+/*Redirecting to the home route*/
+router.route('/index').get((req, res) => {
+    res.redirect('/');
+});
+
+/*Redirecting to the home route*/
+
+router.route('/home').get((req, res) => {
+    res.redirect('/');
+});
+
 module.exports = router;
